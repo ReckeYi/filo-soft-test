34cfa9f65556e8a18fd005c3826809a5f086d530
ReckeYi <79193494+ReckeYi@users.noreply.github.com>
2024-09-09 23:45:26 +0300

    small fixes

--- page/google_search_page.py
+++ page/google_search_page.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 from utils.constants import *
-from locators import GoogleLocators
+from page.locators import GoogleLocators
 
 
 class GooglePage:
@@ -17,11 +17,11 @@ class GooglePage:
 
     def accept_cookies(self):
         try:
-            WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, 5).until(
                 EC.element_to_be_clickable(GoogleLocators.ACCEPT_COOKIES_BUTTON)
             ).click()
         except Exception as e:
-            print("Cookies banner not found or was not closed:", e)
+            print(e, "Cookies banner not found or was closed")
 
     def search_phrase(self):
         search_box = self.driver.find_element(*GoogleLocators.SEARCH_BOX)
